c0b65426c6a45a53f83da7d38ae751f3ca5fc4a9
mn <battledamaged888@gmail.com>
2024-03-07 01:15:20 -0600

    version 1.2
    Added VocaDB search
    
    Database functionality implemented (needs further testing)
    The database (database.js) currently references [all videos in this vocarank playlist](https://www.nicovideo.jp/mylist/3678045). These were obtained from grabbing the linked videos in the descriptions of the vocaranks, so there MAY be songs missing, but I was unable to find any songs that were present in the video but not the description during my review (which WAS brief)
    I hope this feature will grow to be game-changing, and soon I plan to expand the database to include all vocaranks I can find that have mylists or videos linked in their descriptions.

--- vocalostutils.user.js
+++ vocalostutils.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         Vocalost Utils for NicoNico
 // @namespace    https://github.com/mn7216/Vocalost
-// @version      1.1
+// @version      1.2
 // @description  Make identifying, searching for, and archiving lost songs easier.
 // @author       MN_7216
 // @match        https://www.nicovideo.jp/watch/*
 // @match        https://www.nicovideo.jp/user/*
 // @grant        none
-// @updateURL   https://raw.githubusercontent.com/mn7216/Vocalost/main/vocalostutils.js
-// @downloadURL https://raw.githubusercontent.com/mn7216/Vocalost/main/vocalostutils.js
-// @supportURL  https://github.com/mn7216/Vocalost/issues
+// @require      https://raw.githubusercontent.com/mn7216/Vocalost/main/vocaranks/database.js
+// @updateURL    https://raw.githubusercontent.com/mn7216/Vocalost/main/vocalostutils.js
+// @downloadURL  https://raw.githubusercontent.com/mn7216/Vocalost/main/vocalostutils.js
+// @supportURL   https://github.com/mn7216/Vocalost/issues
 // ==/UserScript==
 
 (function() {
@@ -30,7 +31,7 @@
     document.body.appendChild(menuContent);
 
     // -- Utility Functions -- //
-
+    // 1. Get sm/nm link
     function getPageBaseId(url) {
         let id = url.split('/').pop();
         if (id.includes('?')) {
@@ -38,19 +39,21 @@
         }
         return id;
     }
-
+    // 2. Only get the numbers after sm/nm (which some sites use instead of the sm/nm)
     function extractNumericId(id) {
         return id.replace(/\D/g, '');
     }
-
+    // 3. define the main features
     function generateTabsContent(isUserPage, baseId, numericId, pageUrl) {
+        const databaseMatch = database.find(entry => entry.id === baseId);
+
         if (isUserPage) {
             return {
                 'User': [
                     { text: 'Nicolog', url: `https://www.nicolog.jp/user/${baseId}` },
                     { text: 'PFP 1', url: `https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/${substringForUserIcon(numericId, 3)}/${numericId}.jpg` },
                     { text: 'PFP 2', url: `https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/${substringForUserIcon(numericId, 4)}/${numericId}.jpg` },
-                    { text: 'PFP 3', url: `https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/${substringForUserIcon(numericId, 2)}/${numericId}.jpg` },
+                    { text: 'PFP 3', url: `https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/${substringForUserIcon(numericId, 2)}/${numericId}.jpg` }, // pfp can be stored with 2, 3, or 4 digits of the user's id (possibly more/less)
                     { text: 'Archive.org', url: `https://web.archive.org/web/*/${pageUrl}` }
                 ]
             };
@@ -65,28 +68,40 @@
                     { text: 'Youtube', url: `https://www.youtube.com/results?search_query="${baseId}"` },
                     { text: 'Sogou', url: `https://www.sogou.com/web?query="${baseId}"` },
                     { text: 'Yandex', url: `https://yandex.com/search/?text=+${baseId}` },
-                ]
+                ],
+                'Database': databaseMatch ? [
+                    { text: `Title: ${databaseMatch.title}` },
+                    { text: `Availability: ${databaseMatch.availability}`, url: `https://www.nicolog.jp/watch/${databaseMatch.id}` },
+                    { text: `Vocarank: ${databaseMatch.vocarank}`, url: `https://www.nicovideo.jp/watch/${getVocarankId(databaseMatch.vocarank)}` }
+                ] : [{ text: 'No match found in the database' }]
             };
         }
     }
-
+    // - Main video functions - //
     function generateArchiveLinks(baseId, numericId, pageUrl) {
         return [
             { text: 'Nicolog', url: `https://www.nicolog.jp/watch/${baseId}` },
-            { text: 'Small Thumbnail', url: `http://tn-skr4.smilevideo.jp/smile?i=${numericId}` },
-            { text: 'Large Thumbnail', url: `http://tn.smilevideo.jp/smile?i=${numericId}.L` },
             { text: 'Hatena', url: `https://b.hatena.ne.jp/entry/www.nicovideo.jp/watch/${baseId}` },
             { text: 'Nicopedia', url: `https://dic.nicovideo.jp/t/v/${baseId}` },
-            { text: 'Archive.org', url: `https://web.archive.org/web/*/${getCleanUrl(pageUrl)}` }
+            { text: 'VocaDB', url: `https://vocadb.net/Search?searchType=Song&filter=https%3A%2F%2Fwww.nicovideo.jp%2Fwatch%2F${baseId}` },
+            { text: 'Archive.org', url: `https://web.archive.org/web/*/${getCleanUrl(pageUrl)}` },
+            { text: 'Small Thumbnail', url: `http://tn-skr4.smilevideo.jp/smile?i=${numericId}` },
+            { text: 'Large Thumbnail', url: `http://tn.smilevideo.jp/smile?i=${numericId}.L` },
         ];
     }
-
+    // get rid of annoying NND tracking information
     function getCleanUrl(pageUrl) {
         return pageUrl.split('?')[0];
     }
 
     function substringForUserIcon(id, length) {
         return id.substr(0, length);
+    }
+
+    // Get the video ID of the main video in the vocarank
+    function getVocarankId(vocarank) {
+        const mainVideo = database.find(entry => entry.vocarank === vocarank && entry.availability === 'Main Video');
+        return mainVideo ? mainVideo.id : '';
     }
 
     // -- UI Functions -- //
@@ -131,21 +146,21 @@
         content.setAttribute('style', 'display: none; position: fixed; top: 50%; right: 40px; transform: translateY(-50%); z-index: 10001; background-color: #FFF; border: 1px solid #CCC; border-radius: 5px; padding: 20px; box-shadow: 0 4px 8px rgba(0,0,0,0.2); max-width: 300px;');
     }
 
-function setupTabsContainer(container, tabs, contentDisplay) {
-    container.style.display = 'flex';
-    container.style.flexWrap = 'wrap';
-    container.style.justifyContent = 'center';
-    container.style.padding = '10px';
+    function setupTabsContainer(container, tabs, contentDisplay) {
+        container.style.display = 'flex';
+        container.style.flexWrap = 'wrap';
+        container.style.justifyContent = 'center';
+        container.style.padding = '10px';
 
-    Object.keys(tabs).forEach((tabName, index) => {
-        const tabButton = createTabButton(tabName);
-        tabButton.onclick = () => activateTab(tabButton, tabs[tabName], contentDisplay);
+        Object.keys(tabs).forEach((tabName, index) => {
+            const tabButton = createTabButton(tabName);
+            tabButton.onclick = () => activateTab(tabButton, tabs[tabName], contentDisplay);
 
-        if (index === 0) tabButton.onclick();
+            if (index === 0) tabButton.onclick();
 
-        container.appendChild(tabButton);
-    });
-}
+            container.appendChild(tabButton);
+        });
+    }
 
     function createTabButton(name) {
         const button = document.createElement('button');
@@ -163,45 +178,52 @@ function setupTabsContainer(container, tabs, contentDisplay) {
         updateTabContent(container, items);
     }
 
-function updateTabContent(container, items) {
-    container.innerHTML = '';
-    items.forEach(item => {
-        const button = document.createElement('button');
-        button.setAttribute('style', `
-            width: 100%;
-            padding: 10px;
-            margin: 10px 0;
-            text-align: center;
-            background-color: #f0f0f0;
-            background-image: linear-gradient(rgba(255, 255, 255, 0.5) 2px, transparent 2px),
-                              linear-gradient(90deg, rgba(255, 255, 255, 0.5) 2px, transparent 2px),
-                              linear-gradient(rgba(255, 255, 255, 0.3) 1px, transparent 1px),
-                              linear-gradient(90deg, rgba(255, 255, 255, 0.3) 1px, transparent 1px);
-            background-size: 20px 20px, 20px 20px, 10px 10px, 10px 10px;
-            background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
-            color: #333;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            cursor: pointer;
-            box-shadow: 0 2px 5px rgba(0,0,0,0.2);
-            transition: transform 0.1s ease, box-shadow 0.1s ease;
-         `);
+    function updateTabContent(container, items) {
+        container.innerHTML = '';
+        items.forEach(item => {
+            const button = document.createElement('button');
+            button.setAttribute('style', `
+                width: 100%;
+                padding: 10px;
+                margin: 10px 0;
+                text-align: center;
+                background-color: #f0f0f0;
+                background-image: linear-gradient(rgba(255, 255, 255, 0.5) 2px, transparent 2px),
+                                  linear-gradient(90deg, rgba(255, 255, 255, 0.5) 2px, transparent 2px),
+                                  linear-gradient(rgba(255, 255, 255, 0.3) 1px, transparent 1px),
+                                  linear-gradient(90deg, rgba(255, 255, 255, 0.3) 1px, transparent 1px);
+                background-size: 20px 20px, 20px 20px, 10px 10px, 10px 10px;
+                background-position: -2px -2px, -2px -2px, -1px -1px, -1px -1px;
+                color: #333;
+                border: 1px solid #ccc;
+                border-radius: 5px;
+                cursor: pointer;
+                box-shadow: 0 2px 5px rgba(0,0,0,0.2);
+                transition: transform 0.1s ease, box-shadow 0.1s ease;
+             `);
 
-        button.addEventListener('mouseover', () => {
-            button.style.transform = 'scale(1.03)';
-            button.style.boxShadow = '0 4px 8px rgba(0,0,0,0.3)';
+            if (item.text.startsWith('Title:') || item.text.startsWith('Availability:') || item.text.startsWith('Vocarank:')) {
+                button.style.backgroundColor = '#c0f0c0';
+                button.style.color = '#006600';
+            }
+
+            button.addEventListener('mouseover', () => {
+                button.style.transform = 'scale(1.03)';
+                button.style.boxShadow = '0 4px 8px rgba(0,0,0,0.3)';
+            });
+
+            button.addEventListener('mouseout', () => {
+                button.style.transform = 'scale(1)';
+                button.style.boxShadow = '0 2px 5px rgba(0,0,0,0.2)';
+            });
+
+            if (item.url) {
+                button.addEventListener('click', () => { window.open(item.url, '_blank'); });
+            }
+            button.textContent = item.text;
+            container.appendChild(button);
         });
-
-        button.addEventListener('mouseout', () => {
-            button.style.transform = 'scale(1)';
-            button.style.boxShadow = '0 2px 5px rgba(0,0,0,0.2)';
-        });
-
-        button.addEventListener('click', () => { window.open(item.url, '_blank'); });
-        button.textContent = item.text;
-        container.appendChild(button);
-    });
-}
+    }
 
     function selectFirstTab(content, tabs) {
         const firstTabName = Object.keys(tabs)[0];
